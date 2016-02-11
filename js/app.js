90994e58b140df3b594782b1532e5fdcc95e70e9
ericjphillips <ericj.phillips@gmail.com>
2016-02-11 10:23:43 -0500

    add sample data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 var google;
 var map;
 
+var model = {
+     localBusiness: [{
+          'name': 'Buck Rub Pub',
+          'address': '2253 North Main Street, Pittsburg, NH',
+          'type': 'Restaurant'
+     }, {
+          'name': 'Dancing Bear Pub',
+          'address': '151 Main Street, Colebrook, NH',
+          'type': 'Restaurant'
+     }, {
+          'name': 'Spa Restaurant & Outback Pub',
+          'address': '869 Washington Street, West Stewartstown, NH',
+          'type': 'Restaurant'
+     }, {
+          'name': 'Timeout Tavern Sports Bar / Northland Restaurant',
+          'address': '114 Gale Street, Canaan, VT',
+          'type': 'Restaurant'
+     }, {
+          'name': 'Bear Rock Adventures',
+          'address': '545 Beach Road, Pittsburg, NH',
+          'type': 'Recreation'
+     }, {
+          'name': 'Let\'s Ride Rentals',
+          'address': '978 Diamond Pond Road, Stewartstown, NH',
+          'type': 'Recreation'
+     }
+          ]
+};
+
 function initMap() {
      'use strict';
      map = new google.maps.Map(document.getElementById('map'), {
