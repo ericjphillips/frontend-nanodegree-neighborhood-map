f99e2c4710a40fab63ce9becfc13f94f1fc0d3aa
ericjphillips <ericj.phillips@gmail.com>
2016-02-11 21:35:07 -0500

    add Google places library and make a radar search request

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var geocoder;
 var map;
 
 var model = {
+     center: {lat: 44.2035, lng: -72.5623},
      localBusiness: [{
           'name': 'Buck Rub Pub',
           'address': '2253 North Main Street, Pittsburg, NH',
@@ -35,12 +36,27 @@ function initMap() {
      'use strict';
      geocoder = new google.maps.Geocoder();
      map = new google.maps.Map(document.getElementById('map'), {
-          center: {
-               lat: 45,
-               lng: -71.6711963
-          },
-          zoom: 10
+          center: model.center,
+          zoom: 8
      });
+}
+
+function callback(results, status) {
+     'use strict';
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+          model.breweries = results;
+     }
+}
+
+function loadBreweries() {
+     'use strict';
+     var service, request;
+     service = new google.maps.places.PlacesService(map);
+     request = {
+          bounds: map.getBounds(),
+          keyword: 'Vermont brewery'
+     };
+     service.radarSearch(request, callback);
 }
 
 function codeAddresses() {
