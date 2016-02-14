2e47c60a585401cbc63f30d4ca338b52648ae3b6
ericjphillips <ericj.phillips@gmail.com>
2016-02-14 12:18:50 -0500

    move action to initMap so everything has the same scope and actually works

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
-var google;
-var geocoder;
-var map;
+var google, map, service;
 
-var model = {center: {lat: 44.2035, lng: -72.5623}};
-
-function callback(results, status) {
-	'use strict';
-	if (status === google.maps.places.PlacesServiceStatus.OK) {
-		model.breweries = results;
-	}
-}
-
-function loadBreweries() {
-	'use strict';
-	var service, request;
-	service = new google.maps.places.PlacesService(map);
-	request = {
-		bounds: map.getBounds(),
-		keyword: 'Vermont brewery'
-	};
-	service.radarSearch(request, callback);
-}
+var model = {
+	center: {
+		lat: 44.2035,
+		lng: -72.5623
+	},
+	breweries: []
+};
 
 function initMap() {
 	'use strict';
+
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: model.center,
 		zoom: 8
 	});
+
+	service = new google.maps.places.PlacesService(map);
+
+	function detailedCallback(place, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			model.breweries.push(place);
+		}
+	}
+
+	function callback(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			results.forEach(function (place) {
+				service.getDetails(place, detailedCallback);
+			});
+		}
+	}
+
+	function loadBreweries() {
+		var request = {
+			bounds: map.getBounds(),
+			keyword: 'Vermont brewery'
+		};
+		service.radarSearch(request, callback);
+	}
+
 	map.addListener('idle', loadBreweries);
 }
