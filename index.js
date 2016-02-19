0e92258bf17a38022c957dca154826a4e7ce193d
ericjphillips <ericj.phillips@gmail.com>
2016-02-19 12:41:05 -0500

    use Request
    looking ahead, it will be easier to make requests to APIs requiring OAuth if Request is used
    Request has options built in for handling OAuth requests

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 //Module dependencies
 var express = require('express');
-var httpProxy = require('http-proxy');
+var request = require('request')
 
+//initialize a local server on project directory, listening on port 8080
 var server = express();
-
 server.set('port', 8080);
-
 server.use(express.static(__dirname));
-
 server.listen(server.get('port'), function () {
 	'use strict';
 	console.log('Express server listening on port ' + server.get('port'));
 });
 
-var apiForwardingUrl = 'https://api.brewerydb.com/v2/locations?region=Vermont&key=47f825b1668bd879c371d39ec0abbcf4&format=json';
+//URL string pointing to the BreweryDB API, querying for all Vermont breweries
+var brewerydbURL = 'https://api.brewerydb.com/v2/locations?region=Vermont&key=47f825b1668bd879c371d39ec0abbcf4&format=json';
 
-var apiProxy = httpProxy.createProxyServer({
-	changeOrigin: true
-});
+console.log('Forwarding API requests');
 
-console.log('Forwarding API requests to ' + apiForwardingUrl);
-
+//listen for any requests made to /brewDB/
+//when a request comes through, pipe it to the request function
+//make the query to the BreweryDB API, and pipe response back to client
 server.all('/brewDB/*', function (req, res) {
 	'use strict';
-	console.log("Request made to /brewDB/");
-	apiProxy.web(req, res, {
-		target: apiForwardingUrl
-	});
+	console.log("AJAX request made to brewDB");
+	req.pipe(request({
+		url: brewerydbURL,
+		qs: req.query,
+		method: req.method
+	})).pipe(res);
 });
