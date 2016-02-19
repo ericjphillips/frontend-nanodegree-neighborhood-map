5d86141cf1254abc769abcbf9f1a06d783c3859f
ericjphillips <ericj.phillips@gmail.com>
2016-02-19 14:33:00 -0500

    add listeners for Untappd queries
    having obtained an API key for Untappd, I want to integrate it into the app
    it looked like I would have to use OAuth but I guess not
    I have to ration my quests to their API, however. The limit is 300 an hour.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Module dependencies
 var express = require('express');
-var request = require('request')
+var request = require('request');
 
 //initialize a local server on project directory, listening on port 8080
 var server = express();
@@ -14,16 +14,39 @@ server.listen(server.get('port'), function () {
 //URL string pointing to the BreweryDB API, querying for all Vermont breweries
 var brewerydbURL = 'https://api.brewerydb.com/v2/locations?region=Vermont&key=47f825b1668bd879c371d39ec0abbcf4&format=json';
 
+var untappdOAuth = {
+	client_id: '33F245D792EDB200715AC09193096EE41450F51F',
+	client_secret: 'EE12D87862B5FA959C68C57B362CE9724A828DFC'
+};
+
 console.log('Forwarding API requests');
 
 //listen for any requests made to /brewDB/
 //when a request comes through, pipe it to the request function
 //make the query to the BreweryDB API, and pipe response back to client
-server.all('/brewDB/*', function (req, res) {
+server.all('/brewDB/', function (req, res) {
 	'use strict';
-	console.log("AJAX request made to brewDB");
+	console.log('AJAX request made to BreweryDB');
 	req.pipe(request({
 		url: brewerydbURL,
+		qs: req.query,
+		method: req.method
+	})).pipe(res);
+});
+
+server.all('/untappd/search/', function (req, res) {
+	'use strict';
+	req.pipe(request({
+		url: 'https://api.untappd.com/v4/search/brewery?client_id=' + untappdOAuth.client_id + '&client_secret=' + untappdOAuth.client_secret,
+		qs: req.query,
+		method: req.method
+	})).pipe(res);
+});
+
+server.all('/untappd/brewery/', function (req, res) {
+	'use strict';
+	req.pipe(request({
+		url: 'https://api.untappd.com/v4/brewery/info?client_id=' + untappdOAuth.client_id + '&client_secret=' + untappdOAuth.client_secret,
 		qs: req.query,
 		method: req.method
 	})).pipe(res);
